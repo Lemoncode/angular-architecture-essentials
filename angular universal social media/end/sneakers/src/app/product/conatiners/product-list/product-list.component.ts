affe214e70941c860a2994d786157f3e782e8ecd
jaimesalas <jaime.salas@lemoncode.net>
2018-12-18 22:02:13 +0100

    updated angular universal demo

--- angular universal social media/end/sneakers/src/app/product/conatiners/product-list/product-list.component.ts
+++ angular universal social media/end/sneakers/src/app/product/conatiners/product-list/product-list.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 // import { ProductService } from '../../services/product.service';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators'; // diff
+import { UiService } from '../../../ui/services/ui.service';
 
 @Component({
   selector: 'app-product-list',
@@ -13,14 +14,27 @@ import { map } from 'rxjs/operators';
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private uiService: UiService // diff
+  ) { }
 
   ngOnInit() {
     // this.service.getProducts()
     //   .subscribe(res => this.products = res);
     this.route.data.pipe(
-      map(data => data['products'])
+      map(data => data['products']),
+      tap(products => this.metaData(products))
+    ).subscribe(
+      (res) => this.products = res
     )
+  }
+  // diff
+  metaData(products: Product[]) {
+    this.uiService.setMetaData({
+      title: 'Products',
+      description: 'Check out our sneakers'
+    })
   }
 
 }
