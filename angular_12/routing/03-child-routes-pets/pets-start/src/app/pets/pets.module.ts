64a4570961ea8951ad6fee7160e78b960e44bfd6
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-05-21 20:24:34 +0200

    child routes started

---
+++ angular_12/routing/03-child-routes-pets/pets-start/src/app/pets/pets.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { PetsComponent } from './pets.component';
+import { PetComponent } from './pet.component';
+import { PetsToysComponent } from './pets-toys.component';
+import { PetToysComponent } from './pet-toys.component';
+import { PetInfoComponent } from './pet-info.component';
+import { PetInfoToysComponent } from './pet-info-toys.component';
+import { PetEditComponent } from './pet-edit.component';
+
+@NgModule({
+  declarations: [
+    PetsComponent,
+    PetComponent,
+    PetsToysComponent,
+    PetToysComponent,
+    PetInfoComponent,
+    PetInfoToysComponent,
+    PetEditComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    PetsComponent,
+    PetComponent,
+    PetsToysComponent
+  ]
+})
+export class PetsModule { }
