970a319c3ed085cfea161b2793a4b1acad1684ed
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-05-21 21:11:51 +0200

    added grouping routes

---
+++ angular_12/routing/04-grouping-routes/pets-start/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PageNotFoundComponent } from './page-not-found.component';
+import { WelcomeComponent } from './welcome.component';
+
+import { PetsComponent } from './pets/pets.component';
+import { PetComponent } from './pets/pet.component';
+import { PetsToysComponent } from './pets/pets-toys.component';
+import { PetEditComponent } from './pets/pet-edit.component';
+import { PetInfoComponent } from './pets/pet-info.component';
+import { PetInfoToysComponent } from './pets/pet-info-toys.component';
+
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: '', redirectTo: 'welcome',  pathMatch: 'full' },
+  { path: 'pets', component: PetsComponent },
+  { path: 'pets/:id', component: PetComponent },
+  { path: 'pets/:id/toys', component: PetsToysComponent },
+  {
+    path: 'pets/:id/edit',
+    component: PetEditComponent,
+    children: [ // [2]
+      { path: '', redirectTo: 'info', pathMatch: 'full' },
+      { path: 'info', component: PetInfoComponent },
+      { path: 'toys', component: PetInfoToysComponent },
+    ],
+  },
+  { path: '**', component: PageNotFoundComponent }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
