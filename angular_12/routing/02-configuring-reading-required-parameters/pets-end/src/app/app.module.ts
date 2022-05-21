e5e445acdcce4883ba41a3b096d198dad45c8531
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2022-05-21 20:04:09 +0200

    refactor using observables

--- angular_12/routing/02-configuring-reading-required-parameters/pets-end/src/app/app.module.ts
+++ angular_12/routing/02-configuring-reading-required-parameters/pets-end/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { WelcomeComponent } from './welcome.component';
-import { PetsComponent } from './pets.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PetsModule } from './pets/pets.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
     WelcomeComponent,
-    PetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PetsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular_12/routing/02-configuring-reading-required-parameters/pets-end/src/app/pets/pets.component.ts
+++ angular_12/routing/02-configuring-reading-required-parameters/pets-end/src/app/pets/pets.component.ts
@@ -4,20 +4,20 @@ import { PetsService } from './pets.service';
 @Component({
   selector: 'app-pets',
   template: `
-    <p *ngFor="let pet of pets">
-      {{ pet.name }}
-    </p>
+    <ul>
+      <li *ngFor="let pet of pets">
+        <a [routerLink]="['/pets', pet.name]">{{ pet.name }}</a>
+      </li>
+    </ul>
   `,
   styles: [
   ]
 })
 export class PetsComponent implements OnInit {
-  pets: any[] = [];
-  
+  pets: any;
   constructor(private petsService: PetsService) { }
 
   ngOnInit(): void {
     this.pets = this.petsService.fetchPets();
   }
-
 }
