f48ce672c55204bb99bd3c7a92fae9e127805a7c
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2023-01-07 23:05:47 +0100

    dependency lookup demo added

---
+++ angular_13/services/07-control-dependency-lookup/person-directory-end/src/app/person.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Optional, SkipSelf, Host } from '@angular/core';
+import { LoggerService, loggerFactory } from './logger.service';
+
+@Component({
+  selector: 'app-person',
+  template: `
+    <div style="border:1px;">
+      <p *ngIf="logger === null">No logger</p>
+      <button (click)="doLog()">write log</button>
+    </div>
+  `,
+  styles: [
+  ],
+  providers: [
+    {
+      provide: LoggerService,
+      useFactory: loggerFactory('PersonComponent')
+    }
+  ]
+})
+export class PersonComponent implements OnInit {
+
+  constructor(@Host() @Optional() public logger: LoggerService) { }
+
+  ngOnInit(): void {
+  }
+
+  doLog() {
+    if (this.logger) {
+      this.logger.log('Message from person component');
+    } else {
+      console.log('no logger available');
+    }
+  }
+
+}
---
+++ angular_13/services/07-control-dependency-lookup/person-directory-start/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h2>Logger Service</h2>
+    <app-person></app-person>
+  `,
+})
+export class AppComponent {
+  title = 'person-directory';
+}
