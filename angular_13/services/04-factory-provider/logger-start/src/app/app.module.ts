c686f06703e0079199e9a87cfb742adecec3862d
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2023-01-07 21:44:22 +0100

    added factory provider

---
+++ angular_13/services/04-factory-provider/logger-start/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PersonComponent } from './person.component';
+import { LoggerService } from './service/logger.service';
+import { NewLoggerService } from './service/new-logger.service';
+
+const simpleLogger = {
+  log(msg: string) {
+    console.log(`I am a simple logger: ${msg}`);
+  }
+}
+
+@NgModule({
+  declarations: [AppComponent, PersonComponent],
+  imports: [BrowserModule],
+  providers: [LoggerService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
