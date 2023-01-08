c3e68c3ae13851f966e7229a65c504bb6caeb3bf
jaimesalas <jaime.salas@lemoncode.net>
2023-01-08 22:19:08 +0100

    retry failed added

---
+++ angular_13/http-communication/02-retry-failed/pets-start/src/app/pets.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { PetModel } from './pet.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetsService {
+  private baseUrl = '/api/v1/pets';
+
+  constructor(private http: HttpClient) {}
+  
+  fetchPets(): Observable<PetModel[]> {
+    return this.http.get<PetModel[]>(this.baseUrl);
+  }
+
+  fetchPetById(id: string): Observable<PetModel> {
+    return this.http.get<PetModel>(
+      `${this.baseUrl}/${id}`
+    );
+  }
+}
