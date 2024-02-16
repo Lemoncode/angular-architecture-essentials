be1dece343480636acc2c4917a5e6776f1c7dd0d
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2024-02-16 17:22:35 +0100

    services/favtory migrated

---
+++ angular_17/services/04-factory-provider/logger-start/src/app/service/logger.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class LoggerService {
+
+  constructor() { }
+
+  log(msg: string) {
+    console.log(`logger: ${msg}`);
+  }
+}
